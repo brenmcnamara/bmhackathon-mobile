049a060bd36658c3bc68a8bf42c066a267c43979
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-01-21 08:06:21 +0100

    Creates victory screen

--- src/components/GameOverScreen.react.js
+++ src/components/GameOverScreen.react.js
@@ -1,16 +1,22 @@
 /* @flow */
 
 import GameFooter, { Height as GameFooterHeight } from './GameFooter.react';
+import GoldCoinIcon from '../../assets/GoldCoin-100x100.png';
 import Header from './Header.react';
 import QuestionDots from './QuestionDots.react';
 import React, { Component } from 'react';
 
 import nullthrows from 'nullthrows';
-import uuid from 'uuid/v4';
 
+import {
+  Animated,
+  Easing,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { connect } from 'react-redux';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import { upsertSubmission } from '../actions/game';
 
 import type { Game } from '../models/Game';
 import type { ID } from '../models/types';
@@ -28,13 +34,79 @@ export type Props = {
   user: User,
 };
 
+export type State = {
+  transitionStage: 'IN' | 'TRANSITION_IN' | 'OUT',
+};
+
 class GameOverScreen extends Component<Props> {
+  state: State = {
+    transitionStage: 'OUT',
+  };
+
+  _transitionValue: Animated.Value = new Animated.Value(0);
+
+  componentDidMount(): void {
+    this.setState({ transitionStage: 'TRANSITION_IN' });
+    Animated.timing(this._transitionValue, {
+      easing: Easing.out(Easing.cubic),
+      duration: 1000,
+      delay: 200,
+      toValue: 1.0,
+    }).start(() => {
+      this.setState({ transitionStage: 'IN' }, this._loop);
+    });
+  }
+
   render() {
     const { game } = this.props;
+    const { transitionStage } = this.state;
+    const iconStyles = [
+      styles.icon,
+      {
+        opacity: transitionStage === 'IN' ? 1.0 : this._transitionValue,
+        transform: [
+          {
+            translateY: this._transitionValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [this.state.transitionStage === 'IN' ? 30 : 300, 0],
+            }),
+          },
+        ],
+      },
+    ];
+    const textContainerStyles = [
+      styles.textContainer,
+      {
+        opacity: transitionStage === 'IN' ? 1.0 : this._transitionValue,
+        transform:
+          transitionStage === 'IN'
+            ? []
+            : [
+                {
+                  translateY: this._transitionValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [100, 0],
+                  }),
+                },
+              ],
+      },
+    ];
     return (
       <View style={styles.root}>
         <Header />
-        <View style={styles.gameOverContainer} />
+        <View style={styles.gameOverContainer}>
+          <Animated.View style={textContainerStyles}>
+            <Text style={styles.text}>
+              Congrats! You won a Bayern Coin! Visit the Bayern Munich Store to
+              redeem
+            </Text>
+          </Animated.View>
+          <Animated.Image
+            resizeMode="contain"
+            source={GoldCoinIcon}
+            style={iconStyles}
+          />
+        </View>
         <View style={styles.questionDotsContainer}>
           <QuestionDots
             questions={this.props.inactiveQuestions}
@@ -48,49 +120,21 @@ class GameOverScreen extends Component<Props> {
     );
   }
 
-  _onSelectOption = (index: number, pointValue: number): void => {
-    const submission =
-      this.props.activeSubmission ||
-      createSubmission(
-        this.props.user,
-        this.props.game,
-        nullthrows(this.props.activeQuestion),
-      );
-    this.props.dispatch(
-      upsertSubmission({ ...submission, pointValue, predictionIndex: index }),
-    );
-  };
-}
-
-function createSubmission(
-  user: User,
-  game: Game,
-  question: Question,
-): Submission {
-  const now = new Date();
-  return {
-    createdAt: now,
-    gameRef: {
-      pointerType: 'Game',
-      refID: game.id,
-      type: 'POINTER',
-    },
-    id: uuid(),
-    modelType: 'Submission',
-    pointValue: 0,
-    predictionIndex: 0,
-    type: 'MODEL',
-    updatedAt: now,
-    questionRef: {
-      pointerType: 'Question',
-      refID: question.id,
-      type: 'POINTER',
-    },
-    userRef: {
-      pointerType: 'USER',
-      refID: user.id,
-      type: 'POINTER',
-    },
+  _loop = (): void => {
+    if (this.state.transitionStage !== 'IN') {
+      return;
+    }
+    Animated.timing(this._transitionValue, {
+      toValue: 0.0,
+      duration: 3000,
+      easing: Easing.out(Easing.quad),
+    }).start(() => {
+      Animated.timing(this._transitionValue, {
+        toValue: 1.0,
+        duration: 3000,
+        easing: Easing.out(Easing.quad),
+      }).start(this._loop);
+    });
   };
 }
 
@@ -120,7 +164,9 @@ const styles = StyleSheet.create({
   },
 
   gameOverContainer: {
+    alignItems: 'center',
     flex: 1,
+    justifyContent: 'center',
     marginTop: 32,
   },
 
@@ -131,5 +177,17 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     paddingBottom: GameFooterHeight,
+  },
+
+  textContainer: {
+    alignItems: 'center',
+    marginBottom: 32,
+    marginHorizontal: 24,
+  },
+
+  text: {
+    color: '#666',
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
